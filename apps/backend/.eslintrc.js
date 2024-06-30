e6ccc75287c116159cd182a46a9a275ee624054b
Jannatin Naim <jannatinnaim@MacBookPro.local>
2024-06-30 21:52:03 +0600

    (backend): Add Swagger API documentation and request logging

--- apps/backend/.eslintrc.js
+++ apps/backend/.eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
         node: true,
         jest: true,
     },
-    ignorePatterns: [".eslintrc.js"],
+    ignorePatterns: [".eslintrc.js", "webpack.config.js"],
     rules: {
         "prettier/prettier": "warn",
         "@typescript-eslint/interface-name-prefix": "off",
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { VersioningType } from "@nestjs/common";
+import * as morgan from "morgan";
+import { DocumentBuilder, SwaggerCustomOptions, SwaggerDocumentOptions, SwaggerModule } from "@nestjs/swagger";
+import metadata from "./lib/swagger/metadata";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.use(morgan("tiny"));
 
     app.enableCors({
         origin: true,
@@ -14,6 +19,25 @@ async function bootstrap() {
         type: VersioningType.URI,
         defaultVersion: "1",
     });
+
+    const swaggerDocumentConfig = new DocumentBuilder()
+        .setTitle("Brand Kickstart")
+        .setDescription("Kickstart your brand's app ecosystem.")
+        .setVersion("0.0.1")
+        .build();
+    const swaggerDocumentOptions: SwaggerDocumentOptions = {
+        operationIdFactory: (_: string, methodKey: string) => methodKey,
+    };
+
+    await SwaggerModule.loadPluginMetadata(metadata);
+
+    const swaggerDocument = SwaggerModule.createDocument(app, swaggerDocumentConfig, swaggerDocumentOptions);
+
+    const swaggerSetupOptions: SwaggerCustomOptions = {
+        explorer: true,
+        customSiteTitle: "Brand Kickstart | Swagger API Spec",
+    };
+    SwaggerModule.setup("docs", app, swaggerDocument, swaggerSetupOptions);
 
     await app.listen(4200);
 }
